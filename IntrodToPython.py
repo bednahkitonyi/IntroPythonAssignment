c53ea19c052f090481430ab4f18bd40e8b29a2c0
bednahkitonyi <bednahkitonyi@gmail.com>
2025-03-16 20:15:46 +0300

    Completed assignment for introduction to python

---
+++ IntrodToPython.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number :"))
+
+a = num1 + num2
+b = num1*num2
+c = num1 - num2
+d = num1/num2
+e = num2%num1
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
